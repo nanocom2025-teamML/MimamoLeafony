e4cd83770b2f4a7b3b66c9f75a5cb4db20672e70
Yukito-Aomi <yukito.aomi0726@gmail.com>
2025-08-27 13:59:29 +0900

    Update files

--- main.py
+++ main.py
@@ -1,12 +1,63 @@
 # -*- coding: utf-8 -*-
 
 
-from fastapi import FastAPI
+import os
+
+from dotenv import load_dotenv
+
+from fastapi import Depends, FastAPI
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
+
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, MimamoLeafony!"}
+
+
+@app.post("/users/")
+def create_user(name: str, db: Session = Depends(get_db)):
+    user = User(name=name)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+@app.get("/users/")
+def read_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
