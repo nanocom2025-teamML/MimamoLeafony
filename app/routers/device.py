a0cd574704f00b0b669ef93bec8a80e3c1d35af1
Yukito-Aomi <yukito.aomi0726@gmail.com>
2025-09-09 04:06:40 +0900

    Update post_device_message function
    音声認識の結果のテキスト内に「いってきます」あるいは「ただいま」が含まれる場合に記録する．

--- app/routers/device.py
+++ app/routers/device.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from datetime import datetime
 
 from app.database import get_db
-from app.models import Message
+from app.models import Access, Message
 from app.services.recognize_audio_service import convert_to_wav, recognize_voice
 
 
@@ -11,12 +11,43 @@ router = APIRouter(prefix="/api/device", tags=["device"])
 
 
 @router.post("/messages")
-def post_device_message(data: dict):
+def post_device_message(data: dict, db: Session = Depends(get_db)):
     audio_data = data["audio_data"]
     text: str = recognize_voice(convert_to_wav(audio_data))
     print(text)
 
-    return text
+    # text = "いってきます"
+
+    now = datetime.utcnow()
+
+    if any(word in text for word in ["いって", "行って", "きます", "来ます"]):
+        # 新規Accessの作成
+        new_access = Access(
+            target_id=1,
+            gone_at=now,
+            come_at=None,
+        )
+        db.add(new_access)
+        db.commit()
+        db.refresh(new_access)
+
+    elif any(word in text for word in ["ただ", "いま"]):
+        # 直近のAccessの参照
+        last_access = (
+            db.query(Access)
+            .filter(Access.come_at.is_(None))
+            .order_by(Access.gone_at.desc())
+            .first()
+        )
+
+        if last_access:
+            last_access.come_at = now
+            db.commit()
+            db.refresh(last_access)
+        else:
+            pass
+
+    return {"text": text}
 
 
 @router.post("/touch")
