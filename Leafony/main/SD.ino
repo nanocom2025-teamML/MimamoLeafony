33024a560f31c22331d1badaebd585247ffa39ac
k23061kk <k23061kk@aitech.ac.jp>
2025-09-25 22:41:59 +0900

    Update SD.ino

--- Leafony/main/SD.ino
+++ Leafony/main/SD.ino
@@ -1,24 +1,35 @@
 // VSPI
-// == RTC & microSD Leaf==
+// == RTC & microSD==
 #define SD_EN 14  // D6  IO14
 #define SS 5      // D10 IO5
 #define MOSI 23   // D11 IO23
 #define MISO 19   // D12 IO19
 #define SCK 18    // D13 IO18
 // SDカードの初期化
-void setupSD() {
+bool setupSD() {
+  if (!useSD) {
+    return false;
+  }
   if (!SD.begin()) {
-    progStop("SD card Mount Failed");  // 初期化失敗時は停止
+    systemLog("SD card Mount Failed", 3, 3);  // 初期化失敗時は停止
+    useSD = false;
+    return false;
   }
   systemLog("SD CARD", "OK", 1, 1);
   cardInfo();
+  return true;
 }
 
 // SDカード情報表示
-void cardInfo() {
+bool cardInfo() {
+  if (!useSD) {
+    return false;
+  }
   uint8_t cardType = SD.cardType();
   if (cardType == CARD_NONE) {
-    progStop("No SD card Attached");
+    systemLog("No SD card Attached", 3, 3);
+    useSD = false;
+    return false;
   }
   const char *type;
   if (cardType == CARD_MMC) {
@@ -30,27 +41,37 @@ void cardInfo() {
   } else {
     type = "UNKNOWN";
   }
-  systemLog("SD Card Type", type, 1, 2);
-
-  Serial.printf("-- SD: %lluMB ", SD.cardSize() / (1024 * 1024));
-  Serial.printf("Total: %lluMB ", SD.totalBytes() / (1024 * 1024));
-  Serial.printf("Used: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+  systemLog("Card Type", type, 1, 2);
+  systemLog("SD   (MB)", (int)(SD.cardSize() / (1024 * 1024)), 1, 2);
+  systemLog("Total(MB)", (int)(SD.totalBytes() / (1024 * 1024)), 1, 2);
+  systemLog("Used (MB)", (int)(SD.usedBytes() / (1024 * 1024)), 1, 2);
+  return true;
 }
 
 // ファイルを開く（なければ新規作成、追記モード）
-void fileOpen(const String &filename) {
-  dataFile = SD.open(filename, FILE_WRITE);
-  if (!dataFile) {
-    progStop("File Open Failed");
+bool fileOpen(const String &filename, const char *mode) {
+  if (!useSD) {
+    systemLog("SD", "Can't Open File", 3, 3);
+    return false;
   }
-  systemLog("OPEN", filename);
+  dataFile = SD.open("/" + filename, mode);
+  if (!dataFile) {
+    systemLog("SD", "File Open Failed", 3, 3);
+    return false;
+  }
+  systemLog("OPEN", filename, 1, 2);
+  return true;
 }
 
-void fileClose() {
+bool fileClose() {
+  if (!useSD) {
+    return false;
+  }
   if (dataFile) {
     dataFile.close();
-    systemLog("FILE","CLOSE");
+    systemLog("FILE", "CLOSE", 1, 1);
   } else {
-    systemLog("FILE","NOT OPENED");
+    systemLog("FILE", "NOT OPENED", 2, 1);
   }
+  return true;
 }
